67d7e4f39454f83a145e584c0adf393329b2d184
Jean-Marc Delisle <delisle.jeanmarc@hotmail.com>
2022-05-09 12:05:42 -0600

    Switched navbar to more responsive rather than grid, finished off linking pictures;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,8 @@
-function myFunction() {
-    var x = document.getElementById("myNav");
-    if (x.className === "menu") {
-      x.className += " responsive";
-    } else {
-      x.className = "menu";
-    }
+function updatemenu() {
+  if (document.getElementById('responsive-menu').checked == true) {
+    document.getElementById('menu').style.borderBottomRightRadius = '0';
+    document.getElementById('menu').style.borderBottomLeftRadius = '0';
+  }else{
+    document.getElementById('menu').style.borderRadius = '10px';
   }
+}
